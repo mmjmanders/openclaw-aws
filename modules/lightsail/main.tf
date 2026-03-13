833321028117f669dd73ec5f8ac6d9ea3c05815b
Mark Manders <mmjmanders@gmail.com>
2026-03-13 10:47:58 +0100

    Add user data script for Lightsail instance and refactor instance name initialization

--- modules/lightsail/main.tf
+++ modules/lightsail/main.tf
@@ -10,7 +10,8 @@ variable "lightsail_open_ports" {
 }
 
 locals {
-  name = join("-", ["lightsail", var.stage])
+  name                    = join("-", ["lightsail", var.stage])
+  lightsail_instance_name = "${local.name}-instance"
 }
 
 resource "aws_lightsail_key_pair" "key_pair" {
@@ -19,11 +20,16 @@ resource "aws_lightsail_key_pair" "key_pair" {
 }
 
 resource "aws_lightsail_instance" "openclaw" {
-  name              = "${local.name}-instance"
+  name              = local.lightsail_instance_name
   availability_zone = "${var.region}a"
   blueprint_id      = "openclaw_ls_1_0"
   bundle_id         = var.lightsail_bundle_id
   key_pair_name     = aws_lightsail_key_pair.key_pair.name
+  user_data         = <<EOF
+#!/bin/bash
+
+curl -s https://d25b4yjpexuuj4.cloudfront.net/scripts/lightsail/setup-lightsail-openclaw-bedrock-role.sh | bash -s -- ${local.lightsail_instance_name} ${var.region}"
+EOF
 }
 
 resource "aws_lightsail_instance_public_ports" "instance_public_ports" {
